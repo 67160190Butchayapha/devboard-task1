008f3847902c592cde32d65f54e000232eafc434
67160190 <67160190@go.buu.ac.th>
2026-03-24 17:40:44 +0700

    Add PostCount Component.

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,4 +1,5 @@
 import PostCard from "./PostCard";
+import PostCount from "./PostCount";
 
 function PostList({ posts }) {
   return (
@@ -12,11 +13,14 @@ function PostList({ posts }) {
       >
         โพสต์ล่าสุด
       </h2>
+    <PostCount count={posts.length} />
+
       {posts.map((post) => (
         <PostCard key={post.id} title={post.title} body={post.body} />
       ))}
     </div>
   );
 }
+
 
 export default PostList;
